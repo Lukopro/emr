acd6e1baaaf986a775d781d134c3dadd887135b4
nreiffenberger <n.reiffenberger@und.edu>
2026-05-02 15:02:46 -0500

    Update urls.py

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('patient_dashboard/', views.patient_dashboard, name='patient_dashboard'),
     path('clinical_dashboard/', views.clinical_dashboard, name='clinical_dashboard'),
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('create_clinical_staff/', views.create_clinical_staff, name='create_clinical_staff'),
     path('medical_records/', views.medical_records, name='medical_records'),
     path('appointment_scheduling/', views.appointment_scheduling, name='appointment_scheduling'),
     path('patient_profile/', views.patient_profile, name='patient_profile'),
