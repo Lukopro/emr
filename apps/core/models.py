82c8d2aa1a3c642e1b4ee5a4f629e2b9ae255731
Diareya Dzheyms <ljstenerson@gmail.com>
2026-05-05 14:46:24 -0500

    fix imports

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models import Q, UniqueConstraint
 
 class User(AbstractUser):
     class Role(models.TextChoices):
